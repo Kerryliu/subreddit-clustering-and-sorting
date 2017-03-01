05eb6877fcb9ef6f270ec56dedf8f2f6113b2dd3
Kerry Liu <me@kerryliu.com>
2017-03-01 15:53:27 -0500

    Update main.py
    whoops

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ paths = glob('./data/*.csv')
 
 def main():
     if not os.path.exists('relevantTerms.json'):
-        data = parser.get_subreddit_word_counts(paths, save_to_file=True)
+        relevant_terms = parser.get_subreddit_word_counts(paths, save_to_file=True)
     else:
         with open('relevantTerms.json') as data_file:
             relevant_terms = json.load(data_file)
